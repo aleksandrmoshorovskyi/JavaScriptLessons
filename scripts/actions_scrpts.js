309340df2bcc2fb3274edffdaa12f9bf9a792016
aleksandrmoshorovskyi <moshorovskyi.a@icloud.com>
2022-06-07 15:24:12 +0300

    actions2 commit

--- scripts/actions_scrpts.js
+++ scripts/actions_scrpts.js
@@ -4,7 +4,17 @@ function random(number) {
   return Math.floor(Math.random() * (number+1));
 }
 
-btn.onclick = function() {
-  const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
-  document.body.style.backgroundColor = rndCol;
+function changeBackgroundColor() {
+    const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+    document.body.style.backgroundColor = rndCol;
 }
+
+btn.onclick = function() {
+    changeBackgroundColor();
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+    
+    changeBackgroundColor();
+  });
